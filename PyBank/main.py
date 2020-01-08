3132c82fecb85479ac0bfb1c0caebcd5650de2ec
agordongrad <53713522+agordongrad@users.noreply.github.com>
2020-01-07 21:13:46 -0500

    Update main.py
    I've updated my previous code to reflect new code.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,28 @@
 import csv
+import os
 
-with open(budget_data.csv","r") as pybank
-    reader = cvs.reader(pybank)
+budget_file = os.path.join("C:\\Users\\Amanda\\Desktop\\Python-Challenge\\PyBank", "budget_data.csv")
+row_count = sum(1 for row in csv.reader( open(budget_file) ) )
+print("Total Months: %s" %row_count)
 
-print(reader)
 
+row_count = sum(2 for row in csv.reader( open(budget_file) ) )
+
+
+ with open(budget_file, newline = '') as budget:
+     reader = csv.reader(budget)
+     for i in reader:
+         print(i)
+
+     total_months = 0
+    
+     for row in reader:
+         row += 1
+        
+with open(budget_file, newline = '') as budget:
+    reader = csv.reader(budget)
+    header = next(reader)
+    profitnet = 0
+    for row in reader:
+        profitnet += int(row[1])
+    print(profitnet)
