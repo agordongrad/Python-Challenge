4c7933e87e7296a06abf06fd43dcec14672def77
agordongrad <53713522+agordongrad@users.noreply.github.com>
2020-01-10 23:03:52 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,37 +1,34 @@
-import csv
-import os
-
+import os, csv
+from pathlib import Path 
 budget_file = os.path.join("C:\\Users\\Amanda\\Desktop\\Python-Challenge\\PyBank", "budget_data.csv")
-row_count = sum(1 for row in csv.reader( open(budget_file) ) )
-print("Total Months: %s" %row_count)
-
-
-row_count = sum(2 for row in csv.reader( open(budget_file) ) )
-
-
- with open(budget_file, newline = '') as budget:
-     reader = csv.reader(budget)
-     for i in reader:
-         print(i)
-
-     total_months = 0
-    
-     for row in reader:
-         row += 1
-        
-
-      # The net total amount of "Profit/Losses" over the entire period    
-with open(budget_file, newline = '') as budget:
-    reader = csv.reader(budget)
-    header = next(reader)
-    profitnet = 0
-    list_profit = []    
-    for row in reader:
-        #profitnet = profitnet + int(row[1])
-        profitnet += int(row[1])
-        # to append is to add something together. I needed to append list_profit to my row. row[1] = my row that I named plus [1] which is the second column of the csv file
-        list_profit.append(row[1]) 
-
-    print(profitnet)
-    print(list_profit)
-    # next  I need to subtract each value from the other in order to get the avg of the changes in Profit/Losses
+totalMonths = []
+totalProfit = []
+monthlyProfitChange = []
+with open(budget_file,newline="", encoding="utf-8") as budget:
+     csvreader = csv.reader(budget,delimiter=",") 
+     header = next(csvreader) 
+     for row in csvreader:
+        totalMonths.append(row[0])  # creating a an empty list and then dumping data into that list. Jan2010 is the oth index in the array
+        totalProfit.append(int(row[1])) # '867884' second index. Have to put int bc there are no quotes around the number
+#print(totalMonths)
+#print(totalProfit) 
+for i in range(len(totalProfit)-1): # -1 one is to condense from 10 to 9 bc it starts from 0
+    monthlyProfitChange.append(totalProfit[i+1]-totalProfit[i]) 
+maxIncreaseValue = max(monthlyProfitChange)
+maxDecreaseValue = min(monthlyProfitChange)
+#add plus one so you are perfectly alligned. If you do not complete this step you will be one index off.
+maxIncreaseMonth = monthlyProfitChange.index(max(monthlyProfitChange)) + 1
+maxDecreaseMonth = monthlyProfitChange.index(min(monthlyProfitChange)) + 1 
+print(maxIncreaseValue)
+print(maxDecreaseValue)
+print(monthlyProfitChange)
+print(maxIncreaseMonth)
+print(maxDecreaseMonth)
+print(f"Average Change: {(sum(monthly_profit_change)/len(monthly_profit_change))}")
+print("Financial Analysis")
+print("--------------------------")
+print(f"Total Months: {len(totalMonths)}")
+print(f"Total: ${sum(totalProfit)}")
+print(f"Average Change: {(sum(monthly_profit_change)/len(monthly_profit_change))}")
+print(f"Greatest Increase in Profits: {totalMonths[maxIncreaseMonth]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {totalMonths[maxDecreaseMonth]} (${(str(max_decrease_value))})")
